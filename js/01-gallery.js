668eeaaffb71fe5524d9d981d7ef9872a5b5d8ac
Andrey Schavrov <82474084+Andrii-Shavrov@users.noreply.github.com>
2022-02-13 22:34:57 +0200

    +lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+const galleryItemMarkup = createImageGalleryMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup)
 
-console.log(galleryItems);
+galleryContainer.addEventListener('click', onImageClick)
+
+function createImageGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+             <a class="gallery__link" href="${original}"">
+             <img
+                 class="gallery__image"
+                 src="${preview}"
+                 data-source="${original}""
+                 alt="${description}""
+               />
+             </a>
+             </div> `
+    })
+    .join('')
+}
+
+function onImageClick(event) {
+  event.preventDefault()
+  const isGalleryItem = event.target.classList.contains('gallery__image')
+  if (!isGalleryItem) {
+    return
+  }
+  document.addEventListener('keydown', closeModal)
+
+  const modalWindow = basicLightbox.create(
+    `
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width ="800" height = "600">
+    </div>
+    `,
+  )
+
+  showModal()
+  closeModal(event)
+
+  function showModal() {
+    modalWindow.show()
+  }
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      modalWindow.close()
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+// console.log(galleryItems)
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createImageGalleryMarkup(galleryItems),
+)
 
-console.log(galleryItems);
+function createImageGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+             <a class="gallery__link" href="${original}"">
+             <img
+                 class="gallery__image"
+                 src="${preview}"
+                 data-source="${original}""
+                 alt="${description}""
+               />
+             </a>
+             </div> `
+    })
+    .join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionClass: '',
+})
